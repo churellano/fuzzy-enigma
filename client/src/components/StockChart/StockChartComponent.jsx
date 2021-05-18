a0c8f4df870e91a253d0e6904d60d8bb8571b2f7
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2021-05-17 22:02:02 -0700

    Add y-axis line on hover

--- client/src/components/StockChart/StockChartComponent.jsx
+++ client/src/components/StockChart/StockChartComponent.jsx
@@ -13,6 +13,32 @@ class StockChart extends Component {
   }
 
   componentDidMount() {
+    // From https://stackoverflow.com/questions/45159895/moving-vertical-line-when-hovering-over-the-chart-using-chart-js
+    Chart.defaults.LineWithLine = Chart.defaults.line;
+    Chart.controllers.LineWithLine = Chart.controllers.line.extend({
+      draw: function(ease) {
+          Chart.controllers.line.prototype.draw.call(this, ease);
+
+          if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+            var activePoint = this.chart.tooltip._active[0],
+                ctx = this.chart.ctx,
+                x = activePoint.tooltipPosition().x,
+                topY = this.chart.legend.bottom,
+                bottomY = this.chart.chartArea.bottom;
+
+            // draw line
+            ctx.save();
+            ctx.beginPath();
+            ctx.moveTo(x, topY);
+            ctx.lineTo(x, bottomY);
+            ctx.lineWidth = 0.5;
+            ctx.strokeStyle = '#696969';
+            ctx.stroke();
+            ctx.restore();
+          }
+      }
+    });
+
     let ctx = document.getElementById('chart');
     const chart = new Chart(ctx, {
       type: this.props.type,
